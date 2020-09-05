cd3a1cb1c78423f2e6bba8c44531278f8e31232c
Sami Hattab <hattab_sami@yahoo.fr>
2020-09-05 10:49:57 +0200

    add background color

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
