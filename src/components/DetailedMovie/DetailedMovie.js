03e4ca9537bc82479f84973ad8afd8ba6d485227
Sami Hattab <hattab_sami@yahoo.fr>
2020-09-05 11:20:06 +0200

    design improvement for the detailed movie

--- src/components/DetailedMovie/DetailedMovie.js
+++ src/components/DetailedMovie/DetailedMovie.js
@@ -15,28 +15,28 @@ const DetailedMovie = (props) => {
         <article className="detailed">
             {detailedMovie.length > 0 ?
                 <section className="detailedCtnr">
-                    <img className="poster" src={detailedMovie[0].poster} alt={detailedMovie[0].title}/>
+                    <img className="backdrop" src={detailedMovie[0].backdrop} alt={`Affiche secondaire de ${detailedMovie[0].title}`} />
+                    <img className="poster" src={detailedMovie[0].poster} alt={detailedMovie[0].title} />
                     <h1 className="detailedTitle" >{detailedMovie[0].title}</h1>
                     <h2 className="detailedDate">{detailedMovie[0].release_date}</h2>
                     <h3 className="detailedCategories">{detailedMovie[0].categories.join(' / ')}</h3>
                     <p className="detailedDescription">{detailedMovie[0].description}</p>
+                    <div className='btn-ctnr'>
+                        <Link to={`/movie/edit/${detailedMovie[0].id}`} className='modify'>Modifier</Link>
+                        <input className="delete" type="submit" value="Supprimer" onClick={(e) => props.onDelete(e, detailedMovie[0])}></input>
+                    </div>
                     <div >
                         <h2>Liste des acteurs</h2>
                         <div className="actorCtnr">
-                            {detailedMovie[0].actors.map((actor, key) => (< DetailedActors actor={actor} key={key}/>))}
+                            {detailedMovie[0].actors.map((actor, key) => (< DetailedActors actor={actor} key={key} />))}
                         </div>
                     </div>
                     <div >
                         <h2>Films similaires</h2>
                         <div className="similarCtnr">
-                            {detailedMovie[0].similar_movies.map((similar, key) => (< DetailedSimilar similar={similar} key={key}/>))}
+                            {detailedMovie[0].similar_movies.map((similar, key) => (< DetailedSimilar similar={similar} key={key} />))}
                         </div>
                     </div>
-                    <div className='btn-ctnr'>
-                        <Link to={`/movie/edit/${detailedMovie[0].id}`} className='modify'>Modifier</Link>
-                        <input className="delete" type="submit" value="Supprimer" onClick={(e) => props.onDelete(e, detailedMovie[0])}></input>
-                    </div>
-                    <img className="backdrop" src={detailedMovie[0].backdrop} alt={`Affiche secondaire de ${detailedMovie[0].title}`}/>
                 </section>
 
                 :
