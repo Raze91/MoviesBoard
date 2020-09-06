32165e40363fd454213ef491412bd0f7d2238f45
Sami Hattab <hattab_sami@yahoo.fr>
2020-09-06 10:52:06 +0200

    now displays a message when there is no movie in the user's library

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -8,10 +8,14 @@ const Movies = (props) => {
 
     return (
         <section className="movies-ctnr">
-            <h2>Bibliothèque de films</h2>
-            {movieList.map((movie, index) => (
-                <Movie movie={movie} key={index} onDelete={props.onDelete} />
-            ))}
+            {movieList.length > 0 ?
+                <>
+                    <h2>Bibliothèque de films</h2>
+                    {movieList.map((movie, index) => (
+                        <Movie movie={movie} key={index} onDelete={props.onDelete} />
+                    ))}
+                </> : <h2>Il n'y a pas de films à afficher dans votre Bibliothèque</h2>}
+
         </section>
     )
 }
